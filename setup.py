7767289197329802b2332607c8b033023444d9c0
jayanthreddymandadi <mandadijayanth2005@gmail.com>
2025-08-22 15:19:01 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+    setup(
+        name="ML PORJ",
+        version="0.1",
+        author="Jayanth",
+        packages=find_packages(),
+        install_requires=requirements,)    
